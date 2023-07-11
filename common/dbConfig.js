7641bbb24475842d22dc7346f9395123b21c0e5c
krish-1712 <krishkannan1712@gmail.com>
2023-07-11 10:54:26 +0530

    last

--- common/dbConfig.js
+++ common/dbConfig.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const dbUrl=`mongodb+srv://krishkannan1712:Bala199@cluster0.v5ee5wa.mongodb.net/${process.env.DB_NAME}`
+const dbUrl=`mongodb+srv://${process.env.EMAIL}:${process.env.PASSWORD}@cluster0.v5ee5wa.mongodb.net/${process.env.DB_NAME}`
 
 
 module.exports={dbUrl}
